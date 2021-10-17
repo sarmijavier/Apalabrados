340a338c7089463a74fb7cb43850985a82aa3179
Yahuarcani <hjsarmiento1999@gmail.com>
2021-10-17 00:14:52 -0500

    config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config(object):
-    database = 'art_catalogue'
-    server = 'dbitlookssimple.cmsj7x8axmnv.us-east-2.rds.amazonaws.com'
-    username = 'adminitlookssim'
-    password = 'EZpdf2l45xCQCfNlbrtv'
+    database = ''
+    server = ''
+    username = ''
+    password = ''
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://"+username+":"+password+"@"+server+"/"+database
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
